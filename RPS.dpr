0cbe0859985e8781e319d23934c2c52c7813cc77
Alan Colburn <alan.colburn@csulb.edu>
2017-10-24 12:57:47 -0700

    initial commit

---
+++ RPS.dpr
@@ -0,0 +1,14 @@
+program RPS;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  MainU in '..\MainU.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
